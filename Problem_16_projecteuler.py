64f69e28104295e316b6135223481f697f7c3fb5
GabrielDeCastro <30298508+GabrielDCastro@users.noreply.github.com>
2019-08-06 10:31:50 -0300

    Resolve int problem

--- Problem_16_projecteuler.py
+++ Problem_16_projecteuler.py
@@ -6,6 +6,6 @@ int(number)
 while number >0: 
     rest = number % 10 # take the number after the comma and insert a variable
     soma = soma+rest #whenever you get the rest sum
-    number = int(number/10) #remove number after comma, until number become 0
+    number = int(number//10) #remove number after comma, until number become 0
 
 print(soma)  
